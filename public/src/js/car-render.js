fb969d0e8e3af313644ab51b74daafa79c570b7e
Majid Kargar <majidkargar01@gmail.com>
2023-04-08 01:30:54 +0430

    refactor(car-render): remove unwanted selectors

--- public/src/js/car-render.js
+++ public/src/js/car-render.js
@@ -12,6 +12,11 @@ export class CarModals {
         ]
     }
     // creates carItem div
+    /**
+     * creates a div and puts car names in it
+     * @param {number} itemNum - number of item that wants to be rendered
+     * @returns {Element} div of items
+     */
     createItem(itemNum) {
         // item name
         const item = document.createElement("div")
@@ -19,21 +24,25 @@ export class CarModals {
         item.textContent = this.carModelsArray[itemNum]
         return item
     }
-    // renders the cars inside drop down menu
+    /**
+     * renders the cars inside drop down menu using createItem method
+     * sets the name of the car as the selectors text content after clicking
+     */
     renderCars() {
         // selectors
-        const carModelItemsBox = document.querySelector('.drop-down'),
+        const
             selection = document.querySelector('.selection');
 
-        // renders cars into DOM
+        // renders cars into DOM 
+        //using createItem method to create div
         for (let i = 0; i < this.carModelsArray.length; i++) {
-            carModelItemsBox.append(this.createItem(i))
+            selection.children[1].append(this.createItem(i))
         }
         // selects items after being created
         let items = document.querySelectorAll(".item");
-
-        // events
         
+        // events
+
         // click event on items
         items.forEach(item => {
             // sets the items text content as the selectors text content
