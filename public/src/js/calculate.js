ca9a58b1bfacda0206c43b662e0ea5a7d25bcdeb
Majid Kargar <majidkargar01@gmail.com>
2023-04-08 20:35:12 +0430

    refactor(calculate): change Insurance calculation and add comments

--- public/src/js/calculate.js
+++ public/src/js/calculate.js
@@ -1,13 +1,24 @@
+// imports
 import addCommaToNumber from "./helpers/comaInBetweenNumbers.js"
 import PersianDate from "./year.js"
+/** 
+ * calculates the Insurance based on given values from dropdown menus in DOM
+ * 
+ */
 class CalculateInsurance {
+    /**
+     * calls the renderFactor method
+     * @param {string} carName - car's name
+     * @param {number} carRegistrationYear - car's registration year number
+     * @param {string} insuranceType - type of insurance
+     */
     constructor(carName, carRegistrationYear, insuranceType) {
         this.carName = carName
         this.carRegistrationYear = carRegistrationYear
         this.insuranceType = insuranceType
-
         this.renderFactor()
     }
+    /** renders the factor inside DOM */
     renderFactor() {
         // selectors
         const
@@ -21,7 +32,7 @@ class CalculateInsurance {
             carRegisterYear.innerHTML = this.carRegistrationYear
             carInsuranceType.innerHTML = this.insuranceType
             calculateHint.classList.add("hide")
-            this.calulate()
+            this.calculate()
             this.toggleVisibility()
         } else {
             // removes the class list "hide" to show the item
@@ -34,6 +45,7 @@ class CalculateInsurance {
             calculateHint.classList.add("animation")
         }
     }
+    /** toggles the visibility of elements */
     toggleVisibility() {
         const
             factorWrapper = document.querySelector('#factor-wrapper'),
@@ -47,16 +59,17 @@ class CalculateInsurance {
         }, 2000)
     }
 
-    calulate() {
+    /** calculates the amount of insurance */
+    calculate() {
+
         // cost holder (div)
         const carInsuranceCost = document.querySelector('#car-insurance-cost')
-        let currentYear = new PersianDate().year()
-        let yearDifference = ((currentYear - this.carRegistrationYear) === 0 ? 0.9 : currentYear - this.carRegistrationYear)
-
+        /** base variables */
+        this.currentYear = new PersianDate().year()
+        this.yearDifference = ((this.currentYear - this.carRegistrationYear) === 0 ? 0.9 : this.currentYear - this.carRegistrationYear)
         this.carRatio = 0
         this.baseCost = 1_000_000
         this.totalCost = 0
-        this.yearRatio = (yearDifference) * 0.5
 
         //  sets the car Ratio
         if (this.carName === 'سورن پلاس') {
@@ -72,27 +85,30 @@ class CalculateInsurance {
             this.carRatio = 1.3
         }
         //  checks for the insuranceType
+        /** if it is thirdPerson this code will be executed */
         if (this.insuranceType === 'شخص ثالث') {
             this.thirdPersonInsurance()
-        } else {
+        }
+        /** if it is body Insurance this code will be executed */
+        else {
             this.bodyInsurance()
         }
+        // calculates the total cost of insurance
+        this.totalCost = this.carRatio * this.yearRatio * this.baseCost
 
         // outPuts the total cost
         carInsuranceCost.textContent = addCommaToNumber(this.totalCost.toFixed()) + " " + 'تومان'
-
-
-
     }
+    /** updates the year ratio */
     thirdPersonInsurance() {
-        this.totalCost = this.carRatio * this.yearRatio * this.baseCost
+        this.yearRatio = (this.yearDifference) * 0.5
     }
+    /** updates the year ratio */
     bodyInsurance() {
-
+        this.yearRatio = (this.yearDifference) * 0.8
     }
 
 }
-
 export default CalculateInsurance
 
 
