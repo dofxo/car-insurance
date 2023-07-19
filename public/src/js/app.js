7afa9a10a04375b82f5fd4d11b1462898e691700
dofxo <57566624+dofxo@users.noreply.github.com>
2023-07-19 22:22:19 +0430

    refactor(calcbutton): change the event listener

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,67 +1,72 @@
-import CarModals from "./car-render.js"
+import CarModals from "./car-render.js";
 import PersianDate from "./year.js";
 import CalculateInsurance from "./calculate.js";
 
 // selectors
-const
-    selection = document.querySelectorAll('.selection'),
-    insuranceCheckBox = document.querySelectorAll(".insurance-type-check-box");
-
-
+const selection = document.querySelectorAll(".selection"),
+  insuranceCheckBox = document.querySelectorAll(".insurance-type-check-box");
 
 // renders the car List
-new CarModals().renderCars()
+new CarModals().renderCars();
 
 //  renders a "new 20 years from" now list
-new PersianDate().currenYearToNYearsBack(20)
+new PersianDate().currenYearToNYearsBack(20);
 
 // events
 insuranceCheckBox.forEach((checkbox) => {
-    checkbox.addEventListener("click", () => {
-        // Remove "checked" class from all insurance-type-checkboxes
-        insuranceCheckBox.forEach((item) => item.classList.remove("checked"));
+  checkbox.addEventListener("click", () => {
+    // Remove "checked" class from all insurance-type-checkboxes
+    insuranceCheckBox.forEach((item) => item.classList.remove("checked"));
 
-        if (checkbox.classList.contains("checked")) {
-            // Remove "checked" class from clicked checkbox.
-            checkbox.classList.remove("checked");
-        } else {
-            // Add "checked" class to clicked checkbox.
-            checkbox.classList.add("checked");
-        }
-    });
+    if (checkbox.classList.contains("checked")) {
+      // Remove "checked" class from clicked checkbox.
+      checkbox.classList.remove("checked");
+    } else {
+      // Add "checked" class to clicked checkbox.
+      checkbox.classList.add("checked");
+    }
+  });
 });
 
 // click event on selection
-selection.forEach(selection => {
-    selection.addEventListener("click", () => {
-        if (selection.children[1].classList.contains("hide")) {
-            selection.children[1].classList.remove("hide")
-        } else {
-            selection.children[1].classList.add("hide")
-        }
-    })
-})
+selection.forEach((selection) => {
+  selection.addEventListener("click", () => {
+    if (selection.children[1].classList.contains("hide")) {
+      selection.children[1].classList.remove("hide");
+    } else {
+      selection.children[1].classList.add("hide");
+    }
+  });
+});
 
 // gets the values after clicking the calc button and calcs the insurance amount
-const calculateButton = document.querySelector('#calculate-button')
+const calculateButton = document.querySelector("#calculate-button-wrapper");
 
 // call the calculate method in the calculate class
-const insuranceCheckBoxes = document.querySelectorAll('.insurance-type-check-box')
-calculateButton.addEventListener('click', () => {
-    // car model value
-    const carModelNameValue = document.querySelector('#car-model-box > span').textContent
-    // car registration value
-    const carRegistrationYearValue = document.querySelector('#car-registration-year-box > span').textContent
-    // insuranceType 
-    let insuranceTypeValue
-    // choosing the checked insuranceType
-    insuranceCheckBoxes.forEach(
-        item => {
-            if (item.classList.contains('checked')) {
-                insuranceTypeValue = item.nextElementSibling.textContent
-            }
-        }
-    )
-    // gathering the inforamtions for calculating the insurance
-    new CalculateInsurance(carModelNameValue, carRegistrationYearValue, insuranceTypeValue)
-})
+const insuranceCheckBoxes = document.querySelectorAll(
+  ".insurance-type-check-box"
+);
+calculateButton.addEventListener("click", () => {
+  // car model value
+  const carModelNameValue = document.querySelector(
+    "#car-model-box > span"
+  ).textContent;
+  // car registration value
+  const carRegistrationYearValue = document.querySelector(
+    "#car-registration-year-box > span"
+  ).textContent;
+  // insuranceType
+  let insuranceTypeValue;
+  // choosing the checked insuranceType
+  insuranceCheckBoxes.forEach((item) => {
+    if (item.classList.contains("checked")) {
+      insuranceTypeValue = item.nextElementSibling.textContent;
+    }
+  });
+  // gathering the inforamtions for calculating the insurance
+  new CalculateInsurance(
+    carModelNameValue,
+    carRegistrationYearValue,
+    insuranceTypeValue
+  );
+});
